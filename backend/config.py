560ff68db68376f8d97ebfc0bf2016bc2fc0776e
Nati Friedman <nati.freedman@gmail.com>
2025-04-04 11:32:46 +0300

    installed flask and dependencies

---
+++ backend/config.py
@@ -0,0 +1,15 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+# Application's main directory
+basedir = path.abspath(path.dirname(__file__))
+# Specificy a `.env` file containing key/value config values
+load_dotenv(path.join(basedir, '.env'))
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY') or None
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or \
+        'sqlite:///' + path.join(basedir, 'app.db')
+    ENVIRONMENT = environ.get("ENVIRONMENT")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_DEBUG = environ.get("FLASK_DEBUG")
