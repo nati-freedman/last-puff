02af037664840c5db435e2b2c09b9ea04f8a2db2
Nati Friedman <nati.freedman@gmail.com>
2025-04-05 00:47:16 +0300

    feature\login

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,15 @@
 from datetime import date, datetime, timezone
 from typing import Optional
+
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
 # Models: User, Post, Milestone, Progress
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -20,6 +23,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    # Generate hashed password for database storage
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Check password validity
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
@@ -32,3 +43,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+# Load user from database given an ID
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from urllib.parse import urlsplit
 
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app import app, db
+from app.forms import LoginForm
+from app.models import User
 
 # Routes for all users: homepage, about, login/logout
 # Routes for logged in user: user dashboard, feed
@@ -18,11 +22,40 @@ def about():
     return render_template('about.html', title='About')
 
 # Login page
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('logim'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page=url_for('user_dashboard')
+        return redirect(next_page)
     return render_template('login.html', title='Login', form=form)
+
+# Logout user and redirect to the home page
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# User dashboard page
+# TODO: CUSTOM URL
+@app.route('/dashboard')
+@login_required
+def user_dashboard():
+    return render_template('dashboard.html', title='Dashboard')
+
+# User feed page
+# TODO: CUSTOM URL
+@app.route('/feed')
+@login_required
+def user_feed():
+    return render_template('feed.html', title='Feed')
