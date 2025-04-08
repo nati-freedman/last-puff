5a4c64606da91af8f011e34c936c66d894e62152
Nati Friedman <nati.freedman@gmail.com>
2025-04-08 03:37:14 +0300

    added avatars

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -32,11 +32,11 @@ def login():
             sa.select(User).where(User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('logim'))
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlsplit(next_page).netloc != '':
-            next_page=url_for('user_dashboard')
+            next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Login', form=form)
 
@@ -51,14 +51,16 @@ def logout():
 @app.route('/dashboard/<username>')
 @login_required
 def user_dashboard(username):
-    return render_template('dashboard.html', title='Dashboard')
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return render_template('dashboard.html', title='Dashboard', user=user)
 
 # User feed page
 # TODO: CUSTOM URL
 @app.route('/feed/<username>')
 @login_required
 def user_feed(username):
-    return render_template('feed.html', title='Feed')
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return render_template('feed.html', title='Feed', user=user)
 
 # Registration page
 @app.route('/register',methods=['GET', 'POST'])
